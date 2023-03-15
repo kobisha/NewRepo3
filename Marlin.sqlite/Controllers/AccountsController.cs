48cf34a0b03e7c5a755829256c0fafde7e1406e6
giorgi kobauri <gkobauri@bdo.ge>
2023-03-15 15:50:11 +0400

    Add project files.

---
+++ Marlin.sqlite/Controllers/AccountsController.cs
@@ -0,0 +1,46 @@
+﻿using Marlin.sqlite.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Marlin.sqlite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public AccountsController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+
+        public async Task<ActionResult<List<Accounts>>>AddUser(Accounts user)
+        {
+            _context.Accounts.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Accounts.ToListAsync());
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Accounts>>> GetUsers()
+        {
+            return Ok(await _context.Accounts.ToListAsync());
+        }
+
+        [HttpGet("{id]")]
+
+        public async Task<ActionResult<Accounts>>GetUser(int id)
+        {
+            var user = await _context.Accounts.FindAsync(id);
+            if (user == null)
+            {
+                return BadRequest("User not found.");
+            }
+            return Ok(user);
+        }
+    }
+}
---
+++ Marlin.sqlite/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Marlin.sqlite.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Accounts> Accounts => Set<Accounts>();
+    }
+}
